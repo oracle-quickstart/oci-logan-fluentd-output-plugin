d15527f01e9d1b9585f76b6f87d5c4c0f00984d4
admi zakaryae <admizakaryae@admis-MacBook-Pro.local>
2024-09-03 21:31:24 +0100

    fixing last commit issue

--- lib/fluent/plugin/out_oci-logging-analytics.rb
+++ lib/fluent/plugin/out_oci-logging-analytics.rb
@@ -1,6 +1,5 @@
 ## Copyright (c) 2021, 2022  Oracle and/or its affiliates.
 ## The Universal Permissive License (UPL), Version 1.0 as shown at https://oss.oracle.com/licenses/upl/
-require 'json'
 require 'fluent/plugin/output'
 require "benchmark"
 require 'zip'
